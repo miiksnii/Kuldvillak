60abfcc69ffc2e65e28b95cc6bb8fd1603fffa0c
markusmikk100 <tauritammela272@gmail.com>
2025-04-03 11:14:19 +0300

    bottom bar and no scroll

--- src/components/DisplayKuldvillak.vue
+++ src/components/DisplayKuldvillak.vue
@@ -64,56 +64,61 @@ const kuldvillak_data = [
 
 function showModal(question) {
   isModalActive.value = true;
-
 }
-
 </script>
 
 <template>
+  <div class="main-container">
+    <div class="columns">
+      <div class="column kuldvillak-data" v-for="data in kuldvillak_data" :key="data.id">
+        <div class="header box">
+          <p class="header-text is-text-center my-2 py-2 py-5">Hello I am Kaspar Bergert</p>
+        </div>
 
-  <div class="columns">
-
-    <div class="column kuldvillak-data" v-for="data in kuldvillak_data" :key="data.id">
-      <div class="header box">
-        <p class="header-text is-text-center my-2 py-2 py-5">Hello I am Kaspar Bergert</p>
-      </div>
-
-      <div class="numbered-cards">
-        <!-- when the card is clicked shows a modal window -->
-        <div class="numbered-card is-size-2 is-text-center my-2 py-2 py-5" v-for="question, index in data.questionsList"
-          :key="question.id" @click="showModal(data)">
-          ${{ (index + 1) * 100 }}
+        <div class="numbered-cards">
+          <div class="numbered-card is-size-2 is-text-center my-2 py-2 py-5"
+            v-for="question, index in data.questionsList" :key="question.id" @click="showModal(data)">
+            ${{ (index + 1) * 100 }}
+          </div>
         </div>
       </div>
 
-    </div>
-    <div class="modal" :class="{ 'is-active': isModalActive }">
-      <div class="modal-background" @click="isModalActive.value = false"></div>
-      <div class="modal-content">
+      <!-- Modal -->
+      <div class="modal" :class="{ 'is-active': isModalActive }">
+        <div class="modal-background" @click="isModalActive.value = false"></div>
+        <div class="modal-content">
+        </div>
+        <button class="modal-close is-large" aria-label="close"></button>
       </div>
-      <button class="modal-close is-large" aria-label="close"></button>
     </div>
 
+    <!-- Bottom section for names -->
+    <div class="bottom-names">
+      <div class="name-box" v-for="name in ['Kaspar', 'Tauri', 'Martin']" :key="name">
+        {{ name }}
+      </div>
+    </div>
   </div>
 </template>
+
 <style scoped>
 .header {
   background-color: #2f90ff;
   color: rgb(255, 255, 255);
   font-weight: 600;
-  font-size:0.8cm;
-  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 0.8cm;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 
 .numbered-card {
-  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   background-color: #2ca0ffbd;
   border-radius: 10px;
   text-align: center;
   cursor: pointer;
   user-select: none;
   transition: transform 0.2s cubic-bezier(0.1, 3, 0.9, 1);
-  color: rgb(255, 255, 255)
+  color: rgb(255, 255, 255);
 }
 
 .numbered-card:hover {
@@ -122,10 +127,35 @@ function showModal(question) {
 }
 
 .columns {
-  height: 100vh;
+  height: calc(100vh - 100px);
+  /* Adjust height to leave space for bottom section */
 }
 
 .kuldvillak-data {
   height: fit-content;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding-top: 11%;
+}
+
+
+.bottom-names {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px;
+  font-size: 50px;
+  
+}
+
+.name-box {
+  background-color: #ffffffbd;
+  border-radius: 8px;
+  padding: 10px;
+  text-align: center;
 }
 </style>
