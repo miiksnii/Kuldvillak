2ca7fa58c9488b6ca31337513f3f6129addc3190
Kaspar Bergert <50704267035@tptlive.ee>
2025-04-04 13:02:32 +0300

    merge fixed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
- 
+
 import DisplayKuldvillak from './components/DisplayKuldvillak.vue';
- 
- 
+
+
 import Tabs from './components/Tabs.vue';
 import Login from './components/Login.vue';
 import SignIn from './components/SignIn.vue';
@@ -17,13 +17,6 @@ const tabItems = [
   <div class="container is-flex is-justify-content-center is-align-items-center" style="height: 100vh;">
     <DisplayKuldvillak></DisplayKuldvillak>
   </div>
-</template>
- 
-
-
-  <div class="container is-flex is-justify-content-center is-align-items-center" style="height: 100vh;">
-    <DisplayKuldvillak></DisplayKuldvillak>
-  </div>
 
   <section class="hero is-fullheight">
     <div class="hero-body">
@@ -35,7 +28,8 @@ const tabItems = [
   </section>
 </template>
 
-<style scoped>
 
-</style>
 
+
+
+<style scoped></style>
