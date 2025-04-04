6fc1f766fdf62a5afaed57765ad453970297bb64
Martin Pruus <martin.pruus@tptlive.ee>
2025-04-04 12:33:53 +0300

    Add Login & Register system

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,28 +1,58 @@
 <script setup>
+import { ref } from "vue";
 
+const username = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+const message = ref('');
+
+function Register() {
+    if (!username.value) {
+        message.value = 'Please enter a username';
+        return;
+    }
+    if (!password.value || !confirmPassword.value) {
+        message.value = 'Please enter a password and confirm it';
+        return;
+    }
+    if (password.value !== confirmPassword.value) {
+        message.value = 'Passwords do not match';
+        return;
+    }
+    
+    localStorage.setItem('username', username.value);
+    localStorage.setItem('password', password.value);
+    
+    message.value = 'Account successfully created!';
+}
 </script>
 
 <template>
     <div class="field">
         <div class="control">
-            <input class="input" type="text" placeholder="Create username">
+            <input id="usr" v-model="username" class="input" type="text" placeholder="Create username" />
         </div>
     </div>
     <div class="field">
         <div class="control">
-            <input class="input" type="Password" placeholder="Create password">
+            <input id="pw" v-model="password" class="input" type="password" placeholder="Create password" />
         </div>
-    </div><div class="field">
+    </div>
+    <div class="field">
         <div class="control">
-            <input class="input" type="Password" placeholder="Re-enter your password">
+            <input id="rpw" v-model="confirmPassword" class="input" type="password" placeholder="Re-enter your password" />
         </div>
     </div>
     <div class="field">
         <p class="control">
-            <button class="button is-success">
+            <button @click="Register" class="button is-success">
                 Sign in
             </button>
         </p>
+    </div>
+
+    <div v-if="message">
+        {{ message }}
     </div>
 </template>
 
