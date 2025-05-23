39f78e32931436907545b47e7e0f4d3f8e872b0a
Kaspar Bergert <50704267035@tptlive.ee>
2025-05-23 13:33:43 +0300

    fixed mistake

--- src/components/DisplayKuldvillak.vue
+++ src/components/DisplayKuldvillak.vue
@@ -92,10 +92,6 @@ var audio = new Audio(soundFile);
         <div v-for="(question, qIndex) in category.questionsList" :key="question.id"
           class="numbered-card is-size-2 is-text-center my-2 py-2 py-5"
           :class="{ 'is-answered': isAnswered.includes(question.id) }" @click="onQuestionClick(question, qIndex)">
-          :class="{ 'is-answered': isAnswered.includes(question.id) }"
-          @click="onQuestionClick(question, qIndex)
-          audio.play();"
-          >
           <p>${{ (qIndex + 1) * 100 }}</p>
         </div>
       </div>
